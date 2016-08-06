c338e676da0fc3a0e72f4e17a47dddeb48d718c0
Patrick Scott <gitkraken@patrick-scott.com>
2016-08-06 05:15:14 -0700

    Everything was fucked
    My version of Unity has all the fucking animation bugs. Are you kidding me? The Coleman guys say they can't update this system either. I think I'm going to have an aneurysm.
    
    Scripting some anims by hand, I guess.
    
    ....
    
    I did it. It took hours.

---
+++ Assets/Scripts/Goal/FakeGoal.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**Pretend goal to manage the center UI w/o causing script reference confusions -P */
+public class FakeGoal : MonoBehaviour
+{
+	public UnityEngine.UI.Text textUI;
+
+	/**Change the text! */
+	public void UpdateText (Goal goal)
+	{
+		textUI.text = goal.goalText;
+	}
+
+	void Start ()
+	{
+		//We're fake!
+		GetComponent<Animator> ().SetBool ("Fake", true);
+	}
+
+	//Call update text w/ delegate!
+	void OnEnable ()
+	{
+		if (GoalManager.gm)
+			GoalManager.gm.onGoalAdded += UpdateText;
+	}
+
+	void OnDisable ()
+	{
+		if (GoalManager.gm)
+			GoalManager.gm.onGoalAdded -= UpdateText;
+	}
+}
---
+++ Assets/Scripts/SpinToWin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinToWin : MonoBehaviour
+{
+
+	public int angularVelocity;
+
+	void Update ()
+	{
+		transform.localEulerAngles = new Vector3 (transform.localEulerAngles.x,
+			transform.localEulerAngles.y,
+			transform.localEulerAngles.z + angularVelocity);
+	}
+}
