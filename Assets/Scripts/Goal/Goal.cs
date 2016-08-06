b81925f1e63c16b7f6d3c3e0f3e501c5960477e4
Patrick Scott <gitkraken@patrick-scott.com>
2016-08-06 09:30:55 -0700

    Updates

--- Assets/Scripts/Goal/Goal.cs
+++ Assets/Scripts/Goal/Goal.cs
@@ -6,4 +6,9 @@ public class Goal : MonoBehaviour
 	public bool completed = false;
 	public string goalText = "Hey this is Placeholder text";
 	public int pointValue = 1;
+
+	void Start ()
+	{
+		GetComponentInChildren<UnityEngine.UI.Text> ().text = goalText;
+	}
 }
