9dac86463685d48f3b72add2d49cd66bbdb94d3b
Patrick Scott <gitkraken@patrick-scott.com>
2016-08-06 11:32:38 -0700

    TwinkleToes

--- Assets/LOL.cs
+++ Assets/LOL.cs
@@ -3,19 +3,19 @@ using System.Collections;
 
 public class LOL : MonoBehaviour
 {
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start ()
+	{
 
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        gameObject.GetComponent<LOL>();
-    }
+	// Update is called once per frame
+	void Update ()
+	{
+		gameObject.GetComponent<LOL> ();	//Whoever did this is a bastard. Why. Why. -P
+	}
 
-    void FixedUpdate()
-    {
-    }
+	void FixedUpdate ()
+	{
+	}
 }
