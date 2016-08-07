8f88f34bb1b33f3117c996a8e9271f01e79cba74
Patrick Scott <gitkraken@patrick-scott.com>
2016-08-06 21:04:46 -0700

    Cursor and Explosive Balancing

--- Assets/Scripts/Interactable/DestructableObject.cs
+++ Assets/Scripts/Interactable/DestructableObject.cs
@@ -6,90 +6,90 @@ using UnityEngine;
 
 public class DestructableObject : MonoBehaviour
 {
-    private Rigidbody Body;
-    private Collider Hitbox;
+	private Rigidbody Body;
+	private Collider Hitbox;
 
-    public float Durability = DURABILITY_BASE;
+	public float Durability = DURABILITY_BASE;
 
-    private const float DURABILITY_BASE = 50.0f;
+	private const float DURABILITY_BASE = 50.0f;
 
-    private const float EXPLOSION_FORCE_BASE = 100.0f;
-    private const float EXPLOSION_RADIUS_BASE = 5.0f;
-    private const float EXPLOSION_UPWARD = 1.25f;
-    private float Size
-    {
-        get
-        {
-            if (Hitbox != null)
-            {
-                return Hitbox.bounds.size.magnitude;
-            }
-            else
-            {
-                return 1;
-            }
-        }
-    }
+	private const float EXPLOSION_FORCE_BASE = 100.0f;
+	private const float EXPLOSION_RADIUS_BASE = 5.0f;
+	private const float EXPLOSION_UPWARD = 1.25f;
 
-    private void Start()
-    {
+	private float Size
+	{
+		get
+		{
+			if (Hitbox != null)
+			{
+				return Hitbox.bounds.size.magnitude * 4;
+			} else
+			{
+				return 1;
+			}
+		}
+	}
+
+	private void Start ()
+	{
         
-        if (Body == null)
-        {
-            Body = GetComponent<Rigidbody>();
-        }
-        if (Hitbox == null)
-        {
-            Hitbox = GetComponent<Collider>();
-        }
-        Durability = Durability * Size;
-    }
+		if (Body == null)
+		{
+			Body = GetComponent<Rigidbody> ();
+		}
+		if (Hitbox == null)
+		{
+			Hitbox = GetComponent<Collider> ();
+		}
+		Durability = Durability * Size * Mathf.Sqrt (Body.mass);
+	}
 
-    private void Update()
-    {
-        if(Durability < 0)
-        {
-            Explode();
-        }
-    }
+	private void Update ()
+	{
+		if (Durability < 0)
+		{
+			Explode ();
+		}
+	}
 
-    /// <summary>
-    /// Ayy...EXPLOSIONS!
-    /// </summary>
-    public void Explode()
-    {
-        var acolHits = Physics.OverlapSphere(transform.position, EXPLOSION_RADIUS_BASE);
-        foreach(var oHit in acolHits)
-        { 
-            //ignore terrain
-            if(oHit.tag == "Terrain")
-            {
-                continue;
-            }
-            Rigidbody oBody = oHit.GetComponent<Rigidbody>();
-            if(oBody != null)
-            {
-            oBody.AddExplosionForce(EXPLOSION_FORCE_BASE * Size, transform.position, EXPLOSION_RADIUS_BASE * Size, EXPLOSION_UPWARD);
-            }
-        }
-        ExplosionParticleFactory.Instance.Explode(transform.position, Size);
-        Debug.Log(gameObject.name + " has exploded.");
+	/// <summary>
+	/// Ayy...EXPLOSIONS!
+	/// </summary>
+	public void Explode ()
+	{
+		var acolHits = Physics.OverlapSphere (transform.position, EXPLOSION_RADIUS_BASE);
+		foreach (var oHit in acolHits)
+		{ 
+			//ignore terrain
+			if (oHit.tag == "Terrain")
+			{
+				continue;
+			}
+			Rigidbody oBody = oHit.GetComponent<Rigidbody> ();
+			if (oBody != null)
+			{
+				oBody.AddExplosionForce (EXPLOSION_FORCE_BASE * Size, transform.position, EXPLOSION_RADIUS_BASE * Size, EXPLOSION_UPWARD);
+			}
+		}
+		ExplosionParticleFactory.Instance.Explode (transform.position, Size);
+		Debug.Log (gameObject.name + " has exploded.");
 
-        GameObject.Destroy(this.gameObject);
-    }
+		GameObject.Destroy (this.gameObject);
+	}
 
-    private void OnCollisionEnter(Collision a_oCollision)
-    {
-        //no impact with ground...
-        if (a_oCollision.collider.tag == "Terrain")
-        {
-            return;
-        }
-        //we want to determine the force of the other object;
-        Vector3 v3ImpactForce = a_oCollision.impulse;
+	private void OnCollisionEnter (Collision a_oCollision)
+	{
+		//no impact with ground...
+		if (a_oCollision.collider.tag == "Terrain")
+		{
+			return;
+		}
+		//we want to determine the force of the other object;
+		Vector3 v3ImpactForce = a_oCollision.impulse;
 
-        Durability -= v3ImpactForce.magnitude * a_oCollision.collider.bounds.size.magnitude;
-    }
+		Durability -= v3ImpactForce.magnitude * a_oCollision.collider.bounds.size.magnitude;
+	}
 
 }
 
--- Assets/Scripts/Player/Talk.cs
+++ Assets/Scripts/Player/Talk.cs
@@ -5,23 +5,63 @@ public class Talk : MonoBehaviour
 	[SerializeField]
 	private Camera playerView;
 
+	public UnityEngine.UI.Image cursor;
+	private RectTransform cursorRect;
+
+	float lerp = 0;
+	GameObject lastObjHit;
+
 	//Max distance of interaction
 	private const float MAX_INTERACT_DISTANCE = 8.0f;
-	// Use this for initialization
+
 	void Start ()
 	{
-
+		if (cursor)
+			cursorRect = cursor.GetComponent<RectTransform> ();
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-		//Run Talk Script here
-		if (Input.GetKeyUp ("e") || Input.GetMouseButtonDown (0))
+
+		RaycastHit rcHit;
+		//Raycast from center of camera view to a max distance          
+		if (Physics.Raycast (playerView.ScreenPointToRay (new Vector3 (Screen.width / 2, Screen.height / 2)), out rcHit, MAX_INTERACT_DISTANCE)
+		    && InteractableObject.Get (rcHit.collider.gameObject))
 		{
-			RaycastHit rcHit;
-			//Raycast from center of camera view to a max distance          
-			if (Physics.Raycast (playerView.ScreenPointToRay (new Vector3 (Screen.width / 2, Screen.height / 2)), out rcHit, MAX_INTERACT_DISTANCE))
+			//If can interact, indicate
+			if (cursor)
+			{
+				if (InteractableObject.Get (rcHit.collider.gameObject).isActiveAndEnabled)
+				{
+					cursor.color = Color.magenta;
+
+					//Reset
+					lerp = 0;
+				} else if (lastObjHit != rcHit.collider.gameObject)
+				{
+					//Set last obj we hit
+					lastObjHit = rcHit.collider.gameObject;
+
+					//And flash another color
+					cursor.color = Color.yellow;
+
+					lerp = -0.5f;
+				} else if (lerp < 1)
+				{
+					lerp += Time.deltaTime * 4;
+
+					if (lerp > 0)
+						cursor.color = Color.cyan;
+				}
+
+				cursorRect.localScale = Vector3.one * (2 - lerp) / 2;
+			}
+
+
+
+			//Run Talk Script if player is interacting
+			if (Input.GetKeyUp ("e") || Input.GetMouseButtonDown (0))
 			{
 				//Talk Here;
 				GameObject goTarget = rcHit.collider.gameObject;
@@ -29,7 +69,15 @@ public class Talk : MonoBehaviour
 				//tell the object to follow
 				InteractableObject.Follow (goTarget);
 			}
-          
+		} else if (cursor)
+		{
+			if (lerp > 0)
+			{
+				lerp -= Time.deltaTime;
+
+				cursorRect.localScale = Vector3.one * (2 - lerp) / 2;
+				cursor.color = Color.white;
+			}
 		}
 	}
 }
